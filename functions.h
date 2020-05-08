214e1015e715cc268b08b70db6f6f98e512e3c51
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-05-08 19:56:38 +0300

    initial commmit

--- functions.h
+++ functions.h
@@ -1,5 +1,32 @@
 //
 // Created by Albert Mukhametov on 08/05/2020.
 //
+#define MAXCHAR 20
+#define ITERATIONS 100
 
+void questions();
+
+void asknumHex(char numHex[MAXCHAR]);
+
+int removezeroes(char numHex[MAXCHAR], char nozerosHex[MAXCHAR]);
+
+int conversionHex2Dec(char numHex[MAXCHAR]);
+
+int happyNumber(char nozerosHex[MAXCHAR],char matrix[ITERATIONS][5]);
+
+int conversionDec2Hex(int sol, int count, char matrix2[5]);
+
+int squaresum1 (char nozerosHex[MAXCHAR],int end, int count);
+
+void fmatrix(char matrix[ITERATIONS][5], int count);
+
+void count_frequency(int*data,int size);
+
+void numreps(int count,char matfinal[ITERATIONS][5]);
+
+void repetitions(int count);
+
+void showBigaSmallnumH(int count);
+
+int End_program();
 
