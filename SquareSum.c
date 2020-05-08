b8ff80fab0298b9738f7a49aa852068b56c9c97e
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-05-08 20:54:04 +0300

    questions: DONE

---
+++ SquareSum.c
@@ -0,0 +1,21 @@
+//
+// Created by Albert Mukhametov on 08/05/2020.
+//
+
+#include<stdio.h>
+#include<string.h>
+#define MAXCHAR 20
+
+int SquareSum(char nozerosHex[MAXCHAR], int end, int count) {
+
+    return 0;
+}
+
+int main() {
+    char nozerosHex[MAXCHAR];
+    int end;
+    int count;
+
+    int result = SquareSum(nozerosHex, end, count);
+    printf("%i\n", result);
+}
---
+++ conversionDec2Hex.c
@@ -0,0 +1,18 @@
+//
+// Created by Albert Mukhametov on 08/05/2020.
+//
+#include<stdio.h>
+#include<string.h>
+#define MAXCHAR 20
+
+int conversionDec2Hex(int sol, int count, char matrix2[5]) {
+
+}
+
+int main() {
+    int sol;
+    int count;
+    char matrix2[5];
+    int result = conversionDec2Hex(sol, count, matrix2);
+    printf("%i\n", result);
+}
