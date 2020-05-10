5878606b2b513fbf944bb25128b0b3d29bfa32fe
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-05-10 18:27:03 +0300

    valid input TESTED

--- val_input.c
+++ val_input.c
@@ -1,31 +1,74 @@
 //
 // Created by Albert Mukhametov on 10/05/2020.
 //
+#include "functions.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define HEX_LEN 20
+#define MAX_ITERATIONS 100
+#define STR_LEN 5
+
+void printString(char *str) {
+    while (*str != '\0')
+        printf("%c", *str++);
+    printf("\n");
+}
 
 char *capitalise(char *hex) {
     char result[HEX_LEN];
-    for (int i = 0; i < strlen(hex) + 1; i++) {
+    for (int i = 0; i < strlen(hex); i++) {
         result[i] = toupper(hex[i]);
     }
-    char *str = result;
+
+    char *str = malloc(HEX_LEN);
+    for (int i = 0; i < strlen(result); i++) {
+        str[i] = result[i];
+    }
     return str;
 }
 
 char *removeZeros(char *str) {
     char nonZ[HEX_LEN];
     int k = 0;
+    bool leading = true;
     for (int i = 0; i < strlen(str); i++) {
         if (str[i] != '0') {
             nonZ[k] = str[i];
             k++;
+            leading = false;
+        }
+        if (str[i] == '0' && !leading) {
+            nonZ[k] = str[i];
+            k++;
         }
     }
-    char *p = nonZ;
+    char *p = malloc(HEX_LEN);
+    for (int i = 0; i < strlen(nonZ); i++) {
+        p[i] = nonZ[i];
+    }
     return p;
+}
+
+bool allZeros(char *str) {
+    bool zeros = false;
+    while (*str != '\n') {
+        if (*str != '0') {
+            return false;
+        }
+        str++;
+    }
+    return true;
 }
 
 char *askForHexadecimal() {
     char hex[HEX_LEN];
+    char cap[HEX_LEN];
+    char noZeros[HEX_LEN];
     char *str = malloc(HEX_LEN);
 
     printf("Enter a hexadecimal number:\n");
@@ -38,9 +81,15 @@ char *askForHexadecimal() {
     for (int i = 0; i < strlen(hex) - 1; i++) {
         str[i] = hex[i];
     }
+
+
+    printString(hex);
     return str;
 }
 
 int main() {
-    char *hexadecimal = askForHexadecimal();
+//    char *hexadecimal = askForHexadecimal();
+    char *f = removeZeros("0001aacs000");
+    char *c = capitalise(f);
+    printString(c);
 }
