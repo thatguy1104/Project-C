2fefb98f2255536f37cb8fc7c2b06ddc60285fce
thatguy1104 <30369753+thatguy1104@users.noreply.github.com>
2020-05-10 15:41:20 +0300

    iterationLoop added. NOT DONE

--- iterationLoop.c
+++ iterationLoop.c
@@ -9,7 +9,31 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+
 #define HEX_LEN 20
+
+void printString(char *str) {
+    while (*str != '\0')
+        printf("%c", *str++);
+    printf("\n");
+}
+
+int HexToDec(char *hex) {
+    int len = strlen(hex);
+    int base = 1;
+    int dec_val = 0;
+
+    for (int i = len - 1; i >= 0; i--) {
+        if (hex[i] >= '0' && hex[i] <= '9') {
+            dec_val += (hex[i] - 48) * base;
+            base *= 16;
+        } else if (hex[i] >= 'A' && hex[i] <= 'F') {
+            dec_val += (hex[i] - 55) * base;
+            base *= 16;
+        }
+    }
+    return dec_val;
+}
 
 char *DecToHex(int decimal) {
     char hexadecimal[100];
@@ -38,32 +62,54 @@ char *DecToHex(int decimal) {
         final[k] = hexadecimal[j];
         k++;
     }
-    printString(final);
+    final[k] = '\0';
     return str;
 }
 
-int HexToDec(char *hex) {
-    int len = strlen(hex);
-    int base = 1;
-    int dec_val = 0;
-
-    for (int i = len - 1; i >= 0; i--) {
-        if (hex[i] >= '0' && hex[i] <= '9') {
-            dec_val += (hex[i] - 48) * base;
-            base *= 16;
-        } else if (hex[i] >= 'A' && hex[i] <= 'F') {
-            dec_val += (hex[i] - 55) * base;
-            base *= 16;
-        }
+int squaresum1(char *hex) {
+    int Dec2sum = HexToDec(hex);
+    int remainder = 0, sum = 0;
+    while (Dec2sum > 0) {
+        remainder = Dec2sum % 10;
+        sum += remainder * remainder;
+        Dec2sum = Dec2sum / 10;
     }
-    printf("%i\n", dec_val);
-    return dec_val;
+    return sum;
 }
 
 void iterationLoop(char *hex) {
-    
+    int iteration = 1, max_iterations = 100;
+    char continue_key[2];
+    int sum_of_squares = squaresum1(hex);
+    bool start = true;
+    printf("Starting...\n");
+    fgets(continue_key, 1, stdin);
+
+    while (start) {
+        if (iteration != max_iterations && sum_of_squares != 1) {
+            fgets(continue_key, 2, stdin);
+            printf("%s\n", hex);
+            printf("in decimal = %i\n", HexToDec(hex));
+            printf("sum of squares = %i\n", squaresum1(hex));
+            hex = DecToHex(squaresum1(hex));
+            iteration++;
+        } else {
+            start = false;
+            break;
+        }
+    }
+
+
+//    while ((iteration != max_iterations) && (sum_of_squares != 1) && (continue_key[0] == ' ')) {
+//        fgets(continue_key, 1, stdin);
+//        printf("%s\n", hex);
+//        printf("in decimal = %i\n", HexToDec(hex));
+//        printf("sum of squares = %i\n", squaresum1(hex));
+//        hex = DecToHex(squaresum1(hex));
+//        iteration++;
+//    }
 }
 
 int main() {
-    iterationLoop(hexadecimal);
+    iterationLoop("1A");
 }
